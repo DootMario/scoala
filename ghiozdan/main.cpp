0d0de4aca9f4566104d43b9c31a756a4e4dae354
DootMario <115827021+DootMario@users.noreply.github.com>
2023-09-30 21:06:12 +0300

    mare ghiozdan started

---
+++ ghiozdan/main.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ghiozdan{
+
+    int v, m, r=v/m;
+
+};
+
+int main() {
+
+    int n, cap;
+
+    cin>>n>>cap;
+
+    ghiozdan stoc[n];
+
+    for(int i=0; i<n; i++){
+        cin>>stoc[i].m>>stoc[i].v;
+    }
+
+
+
+}
