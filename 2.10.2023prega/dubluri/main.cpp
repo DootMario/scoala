33d5875f963e8d94b9b5a88403eebbae002c6375
DootMario <115827021+DootMario@users.noreply.github.com>
2023-10-08 19:22:18 +0300

    crazy primu ex kinda facut

--- 2.10.2023prega/dubluri/main.cpp
+++ 2.10.2023prega/dubluri/main.cpp
@@ -3,9 +3,34 @@ using namespace std;
 
 ///846
 
+int find(char s[], char t[]){
+    int i=0;
+    char *p;
+    while(strstr(s, t)!=NULL){
+        i++;
+        p = strstr(s, t);
+        strcpy(s+ strlen(s)-strlen(p), s+ strlen(s)-strlen(p)+1);
+    }
+    return i;
+
+}
+
 int main(){
+    char s[256], sub[128];
 
+    cin.getline(s, 256);
 
+    for(int i = strlen(s)/2; i>=2; i--){
+        int j=0;
+        while(j+i<= strlen(s) ){
+            strncpy(sub, s+j, i);
+            if(find(s, sub)>=2){
+                cout<<sub;
+                return 0;
+            }
+            j++;
+        }
+    }
 
 }
 
