322e1799351f999ab78f58ea06b97c30e7abd5fb
DootMario <115827021+DootMario@users.noreply.github.com>
2023-10-09 15:35:00 +0300

    crazy vitan vrea sa plang si sa stau pe math stacks ca l-a gasit pe el

---
+++ crazyVitanixStruct/main.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+float dist(struct pct){
+
+}
+
+struct pct{
+
+    int x=0, y=0, disO=0;
+    char c[30]="origine";
+
+
+};
+
+int main() {
+
+    int n;
+    cin>>n;
+
+    pct v[n];
+
+    for(int i=0; i<n; i++){
+        cin>>v[i].x;
+        cin>>v[i].y;
+        v[i].disO= sqrt(pow(v[i].x, 2) + pow(v[i].x, 2));
+        cin.getline(v[i].c, 30);
+    }
+
+    for(int i=0; i<n; i++){
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                if()
+            }
+        }
+    }
+
+
+}
