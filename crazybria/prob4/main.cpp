22199feff7e496eafd53b923778de9e7da0fda74
DootMario <115827021+DootMario@users.noreply.github.com>
2023-10-10 22:59:14 +0300

    crazy ne testat dar ar trebuii sa mearga gl

---
+++ crazybria/prob4/main.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+
+int main() {
+
+    int m[10][10], x, y, n=0;
+
+    fstream fin("SAH.IN", ios::in);
+
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            fin>>m[i][j];
+        }
+    }
+
+    cin>>x>>y;
+
+    for(int i=0; i<10; i++){
+        for(int j=0; j<10; j++){
+            if((x+y==i+j || x-y==i-j)&&m[i][j]==1){
+                n++;
+            }
+        }
+    }
+
+    if(n==0){
+        cout<<"nu sunt obstacole";
+    }
+
+    cout<<n;
+
+
+
+}
