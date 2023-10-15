d8f9aa9a70a72b3ff9d56337f3f55c2fb5c29376
DootMario <115827021+DootMario@users.noreply.github.com>
2023-10-15 14:27:19 +0300

    crazy 2813 finished

--- 2.10.2023prega/2813/main.cpp
+++ 2.10.2023prega/2813/main.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 int main() {
 
-    char s[101], cuv[51][101], note[7][4]={"DO", "RE", "MI", "FA", "SI", "LA", "SOL"};
+    char s[101], cuv[51][101], note[7][4]={"DO", "RE", "MI", "FA", "SOL", "LA", "SI"};
 
     cin.getline(s, 101);
 
     char* p= strtok(s, " ");
-    strcpy(cuv[0], p);
 
-    int i=1;
+    int i=0;
 
     while (p){
         strcpy(cuv[i], p);
@@ -20,8 +19,9 @@ int main() {
 
     for(int j=0; j<i; j++){
         for(int k=0; k<7; k++){
-            if(strstr(cuv[i], note(k))){
-
+            if(strstr(cuv[j], note[k])){
+                cout<<cuv[j]<<endl;
+                break;
             }
         }
     }
